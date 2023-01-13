5892e9649f358141fb3e0a711b641c0b34955693
unknown <nicholastumminaro@gmail.com>
2023-01-13 15:30:09 -0600

    adds js for search and buttons, needs work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var videoSearch = ""
+
 // sets the api-key when called
 function loadClient() {
   gapi.client.setApiKey("AIzaSyBtiiB2wi4xGoStP5LSQJyym0dMXkHYsY4");
@@ -8,17 +10,53 @@ function loadClient() {
 
 // Make sure the client is loaded before calling this method.
 function execute() {
-  return gapi.client.youtube.search.list({
-    "part": [
-      "id,snippet"
-    ],
-    "q": "Diablo mac miller"
-  })
-      .then(function(response) {
-              // Handle the results here (response.result has the parsed body).
-              console.log("Response", response);
-              console.log(response.result.items[0].id.videoId);
-            },
-            function(err) { console.error("Execute error", err); });
+  // This is to test after 403 error
+  $("li").remove();
+  $("#resultList").append('<li class=""><button id="newPageLink" onclick="nextPage()">' + "test" + '</button></li>');
+  $("#resultList").append('<li class=""><button id="newPageLink" onclick="nextPage()">' + "test" + '</button></li>');
+  $("#resultList").append('<li class=""><button id="newPageLink" onclick="nextPage()">' + "test" + '</button></li>');
+  $("#resultList").append('<li class=""><button id="newPageLink" onclick="nextPage()">' + "test" + '</button></li>');
+  $("#resultList").append('<li class=""><button id="newPageLink" onclick="nextPage()">' + "test" + '</button></li>');
+
+  // return gapi.client.youtube.search.list({
+  //   "part": [
+  //     "id,snippet"
+  //   ],
+  //   "q": videoSearch += " lyrics"
+  // })
+  //     .then(function(response) {
+  //             // clears the list items that are there if any
+  //             $("li").remove();
+  //             // clears the search bar
+  //             $("#default-search")[0].value = "";
+  //             // for all 5 search results, creates a list item
+  //             for (i = 0; i < response.result.items.length; i++) {
+  //               var youtubeTitle = response.result.items[i].snippet.title;
+  //               // add the css style here Charles, I can't figure it out otherwise
+  //               $("#resultList").append('<li class=""><button id="newPageLink">' + youtubeTitle + '</button></li>');
+  //             }
+  //           },
+  //           function(err) { console.error("Execute error", err); });
+            
 }
 gapi.load("client");
+
+// event listener on search button 
+$("#sButton").click(function(e) {
+  e.preventDefault();
+  var searchInfo = $("#default-search")[0].value;
+  console.log(searchInfo);
+  videoSearch = searchInfo;
+  loadClient()
+    .then(function(response){
+      execute();
+    })
+})
+
+$(document).on("click", "newPageLink", function(e){
+  console.log("nut")
+})
+
+// function nextPage() {
+//   window.location.href = "index2.html"
+// }
