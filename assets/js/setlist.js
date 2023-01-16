b13944c233ba73ed730867bf40f0e33d28f6e1b9
anuffisenough <uffmanch@gmail.com>
2023-01-15 20:05:52 -0700

    adds function to create elements and append to ul section

--- assets/js/setlist.js
+++ assets/js/setlist.js
@@ -7,12 +7,33 @@ var userSetListSongSpan = document.querySelector("#set-list-song")
 //function pulls most recent song info from local storage populated by home page search, and sets the text of the li in the set-list ul to the song info string
 function addToSetList() {
     var setListSong = localStorage.getItem("song info");
-
+//checks to see if song info is available in local storage
     if (!setListSong) {
         return;
+
+    } else {
+//if song info is available, stores variable to create an h2
+        var setlistHeading = document.createElement("h2");
+      
+//stores a variable to create an ul
+        var list = document.createElement("ul");
+//stores a variable to create an li
+        var listItem = document.createElement("li");
+//sets text of h2 to "Your Karaoke Set-List"
+        setlistHeading.textContent = "Your Karaoke Set-List"
+//sets the text of the li to "song info" pulled from local storage
+        listItem.textContent = setListSong;
+
+//appends h2 to section
+        document.getElementById("paper-set-list").appendChild(setlistHeading);
+//appends the ul to section
+        document.getElementById("paper-set-list").appendChild(list);
+//appends the li to the ul
+        document.getElementById("paper-set-list").appendChild(listItem);
+        //document.body.section.ul.appendChild(li);
+        //document.body.section.ul.li.appendChild(span);
     }
 
-    userSetListSongSpan.textContent = setListSong;
 }
 
 addToSetlistButton.addEventListener("click", function(event) {
