36691252edd91669ee47acacc8dd90f88100b127
anuffisenough <uffmanch@gmail.com>
2023-01-18 21:13:35 -0700

    gets most of the function updated for the button clicks

--- assets/js/setlist.js
+++ assets/js/setlist.js
@@ -30,5 +30,19 @@ function printToSetList() {
 	}
 }
 
+setListUl.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    localSetList.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    printToSetList();
+    renderTodos();
+  }
+});
 
 printToSetList();
