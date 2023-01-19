ca523d2e674481f160857e030555771760a474a1
anuffisenough <uffmanch@gmail.com>
2023-01-19 13:17:13 -0700

    completes the funcationality for the buttons on set-list page

--- assets/js/setlist.js
+++ assets/js/setlist.js
@@ -2,6 +2,7 @@
 
 //variable for ul element selector
 var setListUl = document.getElementById("set-list")
+
 //variable for the locally-stored song info string
 //variable for the newly created list items
 
@@ -10,39 +11,49 @@ var setListUl = document.getElementById("set-list")
 
 var localSetList = JSON.parse(localStorage.getItem("saved setList"));
 
+//var emptyArray = [];
+
 function printToSetList() {
 		//checks to see if song info is available in local storage
 	//if (localSetList === null) {
         //console.log("You need to add songs!");
     //} else {
 		for (var i = 0; i < localSetList.length; i++) {
-		
         var songEntry = localSetList[i];
         var li = document.createElement("li");
         li.textContent = songEntry;
         li.setAttribute("data-index", i);
 
         var button = document.createElement("button");
-        button.textContent = " X Remove Song";
+        button.textContent = " Remove Song";
 
         li.appendChild(button);
         setListUl.appendChild(li);
 	}
 }
 
+//function clearStoredSetList() {
+ // localStorage.setItem("saved setList", JSON.stringify(emptyArray));
+ // init();
+//}
+
 setListUl.addEventListener("click", function(event) {
   var element = event.target;
-
+  var listItems = $("li");
   // Checks if element is a button
+
+
   if (element.matches("button") === true) {
-    // Get its data-index value and remove the todo element from the list
     var index = element.parentElement.getAttribute("data-index");
     localSetList.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
+    var editedSetList = localSetList;
+    localStorage.setItem("saved setList", JSON.stringify(editedSetList));
+    listItems.remove();
     printToSetList();
-    renderTodos();
-  }
-});
+    // Store updated todos in localStorage, re-render the list
+    //localSetList = JSON.parse(localStorage.getItem("saved setList"));
+    //printToSetList();
+     }
+    });
 
 printToSetList();
