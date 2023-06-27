6e2402df46fa19e19ecf22abef2ae4f44fdb045c
anuffisenough <uffmanch@gmail.com>
2023-06-27 14:14:31 -0600

    updates the youtube API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@
 
 // sets the api-key when called
 function loadClient() {
-	gapi.client.setApiKey("AIzaSyBtiiB2wi4xGoStP5LSQJyym0dMXkHYsY4");
+	gapi.client.setApiKey("AIzaSyDdJlTY2zZWfHrPj2Aype7LzVUnvKmykT0");
 	return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
 		.then(function() { console.log("GAPI client loaded for API"); },
 			function(err) { console.error("Error loading GAPI client for API", err); });
